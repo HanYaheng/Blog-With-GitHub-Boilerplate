46caa9c72ec4c77ec78cdeb13c07a26b6ab54dd1
Hank <46668497+HanYaheng@users.noreply.github.com>
2021-04-14 10:00:56 +0800

    更换主题

--- conf.py
+++ conf.py
@@ -9,10 +9,10 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo",
+    "name": "Prism",
     "type": "git",
-    "url": "https://github.com/AlanDecode/Maverick-Theme-Galileo.git",
-    "branch": "latest"
+    "url": "https://github.com/Reedo0910/Maverick-Theme-Prism.git",
+    "branch": "deploy"
 }
 enable_jsdelivr = {
     "enabled": False,
